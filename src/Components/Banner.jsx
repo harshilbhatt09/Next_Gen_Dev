c06c86dbe794400fa8b85b32737ec3eba1b9bd19
harshilbhatt09 <bhattharshu9503@gmail.com>
2024-06-25 16:53:09 +0530

    initial commit

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <main className='bg-black text-white'>
+        <div className='my-auto text-center'>
+            Grab the best taxi deal
+        </div>
+        <div>
+            <img className='h-96 mx-auto' src="../../src/assets/taxi_main.png"/>
+        </div>
+    </main>
+  )
+}
+
+export default Banner
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaTaxi } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <header className="bg-[#FFFF00]">
+      <nav className="flex justify-between px-6 xl:px-24 py-6 font-oswald">
+        <div className="tracking-wider text-2xl leading-10 flex"><FaTaxi className="mt-2 mr-2" />Akshar Taxi</div>
+        <div className="leading-10">
+          <ul className="flex justify-between space-x-4">
+            <li>Rent a Cab</li>
+            <li>About Us</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
